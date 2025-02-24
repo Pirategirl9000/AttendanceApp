c1b575bf78fca612958ae4986474a8e72f146676
AdamPreston17 <prestonadam84@yahoo.com>
2025-02-24 10:51:30 -0600

    Started student class with some fields and constructors

--- Student.java
+++ Student.java
@@ -1,1 +1,17 @@
+public class Student {
+    private int seat;
+    private String name;
 
+    private Student() {
+        this.seat = 0;
+        this.name = "?";
+    }
+
+    public Student(int seat) {
+        this.seat = seat;
+    }
+
+    public Student(int seat, String name) {
+
+    }
+}
