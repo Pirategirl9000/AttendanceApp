8c460628c369a8d150c61a718bcb7af226ba0fab
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-03-03 15:57:22 -0600

    created setupCourses(), setupStudents(), mainMenu() functions. mainMenu() still needs the switch case completed

--- src/AttendanceApp.java
+++ src/AttendanceApp.java
@@ -1,3 +1,6 @@
+
+// Not sure what you meant by (package private) Course section1 in your documentation. I assume you want us to import things as a package
+// so that is what we did, as far as I know it wouldn't be necessary since they could all go in the default package and there would be no need for importing packages
 import com.example.Input;
 import com.example.Course;
 
@@ -18,7 +21,53 @@ public class AttendanceApp {
         System.out.println(DOUBLE_DASH_LINE);
     }
 
-    private void setUpCourses() {
-
+    private void setupCourses() throws Exception {
+        section1.setName(Input.getLine("Enter Section 1's Course Name: "));
+        section1.setName(Input.getLine("Enter Section 2's Course Name: "));
     }
+
+    private void setupStudents(Course course) throws Exception {
+        while (true) {
+            String name = Input.getLine("Enter " + course.getName() + " Student Name: ");
+
+            if (name.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            int seat = Input.getIntRange("Enter the Seat Number for " + name + ": ", 0, Integer.MAX_VALUE);
+
+            course.addStudent(seat, name);
+        }
+    }
+
+    private void mainMenu() throws Exception {
+        int response;
+
+        while (true) {
+            System.out.println(SINGLE_DASH_LINE);
+            System.out.println("Main Menu");
+            System.out.println(SINGLE_DASH_LINE);
+
+            System.out.println("0 = End Attendance App");
+            System.out.println("1 = Take 9:00 am Attendance");
+            System.out.println("2 = Take 10:00 am Attendance");
+            System.out.println("3 = Display Attendance Report");
+
+            System.out.println(SINGLE_DASH_LINE);
+            response = Input.getIntRange("Menu Choice: ", 0, 3);
+
+            switch (response) {
+                case 0:
+                    return;
+                case 1:
+                    break; //REMOVE LATER
+            }
+
+
+
+
+        }
+    }
+
+
 }
